789acbe88f6b41c0e49faab863ae7333012ecdf0
Dzulfikar Fauzi <fikar.cakap@gmail.com>
2021-06-08 13:23:07 +0700

    input and switch

--- hello_world/lib/input.dart
+++ hello_world/lib/input.dart
@@ -7,6 +7,7 @@ class InputScreen extends StatefulWidget {
 
 class _InputScreenState extends State<InputScreen> {
   String _name = "";
+  var _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +22,126 @@ class _InputScreenState extends State<InputScreen> {
             onChanged: (String value) => setState(() => _name = value),
             onSubmitted: (value) => {},
           ),
+          TextField(
+            controller: _controller,
+          ),
+          InputImplementation(),
+          InputControlImplementation()
         ],
       ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class InputControlImplementation extends StatefulWidget {
+  @override
+  _InputControlImplementationState createState() {
+    return _InputControlImplementationState();
+  }
+}
+
+class _InputControlImplementationState
+    extends State<InputControlImplementation> {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Write your name here...",
+            labelText: "Your Name",
+          ),
+          controller: _controller,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: () => {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Text("Hello, ${_controller.text}"),
+                  );
+                })
+          },
+          child: Text("Submit 2"),
+        ),
+      ],
+    );
+  }
+}
+
+class InputImplementation extends StatefulWidget {
+  @override
+  _InputImplementationState createState() {
+    return _InputImplementationState();
+  }
+}
+
+class _InputImplementationState extends State<InputImplementation> {
+  String _name = "";
+  bool lightOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Write your name here...",
+            labelText: "Your Name",
+          ),
+          onChanged: (String value) => setState(() {
+            _name = value;
+          }),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          onPressed: () => {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Text("Hello, $_name"),
+                  );
+                })
+          },
+          child: Text("Submit"),
+        ),
+        InputControlImplementation(),
+        Switch(
+            value: lightOn,
+            onChanged: (bool value) {
+              setState(() {
+                lightOn = value;
+              });
+
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(lightOn ? "Light On" : "Light Off"),
+                  duration: Duration(seconds: 1),
+                ),
+              );
+            })
+      ],
     );
   }
 }
